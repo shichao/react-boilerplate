337f63b647a4b763e2d19554ffd0fb59fb6de340
Chao Shi <cshi@bgcengineering.ca>
2020-03-16 13:45:31 -0700

    setup route files

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,38 @@
 import * as React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import routes from './routes';
 
 export default function App(props) {
-  return <div>Hello</div>;
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+
+        <Switch>
+          {routes.map((route, i) => (
+            <RouteWithSubRoutes key={i} {...route} />
+          ))}
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function RouteWithSubRoutes(route) {
+  return (
+    <Route
+      path={route.path}
+      render={props => (
+        // pass the sub-routes down to keep nesting
+        <route.component {...props} routes={route.childRoutes} />
+      )}
+    />
+  );
 }
