5f9f715f60c63f282b324e904b8cdc3ffa84b24e
Chao Shi <cshi@bgcengineering.ca>
2020-04-03 13:27:48 -0700

    controls added

--- src/components/FormExample.tsx
+++ src/components/FormExample.tsx
@@ -4,22 +4,19 @@ import * as yup from 'yup';
 import { FormGroup, FormLabel, FormControl } from 'react-bootstrap';
 
 const FormExample = () => {
+  const [isSubmitting, setSubmitting] = React.useState(false);
+
   return (
     <div>
       <h1>Anywhere in your app!</h1>
       <Formik
-        initialValues={{ email: '', password: '' }}
-        validate={values => {
-          const errors: any = {};
-          if (!values.email) {
-            errors.email = 'Required';
-          } else if (
-            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-          ) {
-            errors.email = 'Invalid email address';
-          }
-          return errors;
+        initialValues={{
+          author: '',
+          comment: null,
+          confidence: SiteGaugeAlertConfidence.Unknown,
+          alertIds: [1, 2, 3],
         }}
+        validate={values => {}}
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
             alert(JSON.stringify(values, null, 2));
@@ -38,29 +35,50 @@ const FormExample = () => {
           /* and other goodies */
         }) => (
           <form onSubmit={handleSubmit}>
-            <FormGroup controlId="firstName">
-              <FormLabel>Eamil</FormLabel>
+            <FormGroup>
+              <FormLabel>Confidence</FormLabel>
               <FormControl
-                name="email"
-                type="email"
-                value={values.email}
+                name="confidence"
+                as="select"
+                defaultValue={SiteGaugeAlertConfidence.Unknown}
                 onBlur={handleBlur}
                 onChange={handleChange}
-                isInvalid={!!errors.email}
-              />
+                isInvalid={!!errors.confidence}
+              >
+                <option value={-1}>False</option>
+                <option value={0}>Unknown</option>
+                <option value={1}>Low</option>
+                <option value={2}>Medium</option>
+                <option value={3}>High</option>
+              </FormControl>
               <FormControl.Feedback type="invalid">
-                {errors.email}
+                {errors.confidence}
               </FormControl.Feedback>
             </FormGroup>
-            {errors.email && touched.email && errors.email}
-            <input
-              type="password"
-              name="password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.password}
-            />
-            {errors.password && touched.password && errors.password}
+            <FormGroup>
+              <FormLabel>Author</FormLabel>
+              <FormControl
+                name="author"
+                type="text"
+                value={values.author}
+                onBlur={handleBlur}
+                onChange={handleChange}
+                isInvalid={!!errors.author}
+              />
+              <FormControl.Feedback type="invalid">
+                {errors.author}
+              </FormControl.Feedback>
+            </FormGroup>
+            <FormGroup>
+              <FormLabel>Comment</FormLabel>
+              <FormControl
+                name="comment"
+                type="text"
+                value={values.comment}
+                onBlur={handleBlur}
+                onChange={handleChange}
+              />
+            </FormGroup>
             <button type="submit" disabled={isSubmitting}>
               Submit
             </button>
@@ -72,3 +90,39 @@ const FormExample = () => {
 };
 
 export default FormExample;
+
+enum SiteGaugeAlertConfidence {
+  //confirmed false alert
+  False = -1,
+  //default
+  Unknown = 0,
+  //likely false peak
+  Low = 1,
+  //uncertain
+  Medium = 2,
+  //likely true peak
+  High = 3,
+}
+
+const SiteGaugeAlertValidationSchema = yup.object({
+  author: yup.string().required(),
+  comment: yup.string().nullable(),
+  confidence: yup
+    .mixed<
+      | SiteGaugeAlertConfidence.False
+      | SiteGaugeAlertConfidence.High
+      | SiteGaugeAlertConfidence.Low
+      | SiteGaugeAlertConfidence.Medium
+    >()
+    .oneOf([
+      SiteGaugeAlertConfidence.False |
+        SiteGaugeAlertConfidence.High |
+        SiteGaugeAlertConfidence.Low |
+        SiteGaugeAlertConfidence.Medium,
+    ]),
+  alertIds: yup.array<number>().required(),
+});
+
+type SiteGaugeAlertValidation = yup.InferType<
+  typeof SiteGaugeAlertValidationSchema
+>;
