677af762542d744340e875c247da5c7cfac0a7a4
Chao Shi <cshi@bgcengineering.ca>
2020-09-28 12:56:21 -0700

    bind isopen

--- src/components/form/FormSection.tsx
+++ src/components/form/FormSection.tsx
@@ -2,12 +2,27 @@ import * as React from 'react';
 import { Collapse } from 'react-collapse';
 import { FormSectionSchema } from './FormSchema';
 
+//Styles
+import styles from './FormSection.module.scss';
+
 const FormSection = (props: { schema: FormSectionSchema; children?: any }) => {
   const [isOpened, setIsOpened] = React.useState(
     props.schema.isOpen ? true : false
   );
 
-  return <Collapse isOpened={isOpened}>{props.children}</Collapse>;
+  return (
+    <div className={styles.collapsibleSection}>
+      <div className={styles.header} onClick={() => setIsOpened(!isOpened)}>
+        {props.schema.title}
+        <span
+          className={`${styles.headerIcon} ${
+            isOpened ? 'esri-icon-up' : 'esri-icon-down'
+          }`}
+        ></span>
+      </div>
+      <Collapse isOpened={isOpened}>{props.children}</Collapse>
+    </div>
+  );
 };
 
 export default FormSection;
