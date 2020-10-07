4366a8284271020610e16efa54ad7f75f550059b
Chao Shi <cshi@bgcengineering.ca>
2020-10-07 12:13:47 -0700

    i18n

--- src/app.tsx
+++ src/app.tsx
@@ -3,79 +3,86 @@ import * as yup from 'yup';
 import { FieldType, FormSchema } from './components/cambio/FormSchema';
 import Form from './components/cambio/Form';
 
-const personSchema = yup.object().shape({
-  name: yup.string().required(),
-  age: yup
-    .number()
-    .required()
-    .positive('age must be a positive number')
-    .integer('age must be a positive number'),
-  email: yup.string().email(),
-  website: yup.string().url(),
-  createdOn: yup.date().default(function () {
-    return new Date();
-  }),
-  description: yup.string(),
-});
+import './i18n';
+import { withTranslation } from 'react-i18next';
 
-type Person = yup.InferType<typeof personSchema>;
+const App = ({ t }) => {
+  const personSchema = yup.object().shape({
+    name: yup.string().required(),
+    age: yup
+      .number()
+      .required()
+      .positive(t('age must be a positive number'))
+      .integer(t('age must be a positive number')),
+    email: yup.string().email(),
+    website: yup.string().url(),
+    createdOn: yup.date().default(function () {
+      return new Date();
+    }),
+    description: yup.string(),
+  });
 
-const formSchema: FormSchema = {
-  entitySchema: personSchema,
-  sections: [
-    {
-      title: 'User Summary',
-      rows: [
-        {
-          title: 'Basic Infomation',
-          fields: [
-            { title: 'Name', entityPropertyName: 'name' },
-            { title: 'Age', entityPropertyName: 'age', type: FieldType.Number },
-          ],
-        },
-        {
-          fields: [
-            {
-              title: 'Email',
-              entityPropertyName: 'email',
-              type: FieldType.Email,
-            },
-          ],
-        },
-        {
-          fields: [
-            {
-              title: 'Website',
-              entityPropertyName: 'website',
-              type: FieldType.Text,
-            },
-          ],
-        },
-      ],
-      isOpen: true,
-    },
-    {
-      title: 'Other',
-      rows: [
-        {
-          fields: [
-            {
-              title: 'Description',
-              entityPropertyName: 'description',
-              type: FieldType.Textarea,
-            },
-          ],
-        },
-      ],
-      isOpen: false,
-    },
-  ],
-};
+  type Person = yup.InferType<typeof personSchema>;
 
-const values: Person = {};
+  const formSchema: FormSchema = {
+    entitySchema: personSchema,
+    sections: [
+      {
+        title: 'User Summary',
+        rows: [
+          {
+            title: 'Basic Infomation',
+            fields: [
+              { title: 'Name', entityPropertyName: 'name' },
+              {
+                title: 'Age',
+                entityPropertyName: 'age',
+                type: FieldType.Number,
+              },
+            ],
+          },
+          {
+            fields: [
+              {
+                title: 'Email',
+                entityPropertyName: 'email',
+                type: FieldType.Email,
+              },
+            ],
+          },
+          {
+            fields: [
+              {
+                title: 'Website',
+                entityPropertyName: 'website',
+                type: FieldType.Text,
+              },
+            ],
+          },
+        ],
+        isOpen: true,
+      },
+      {
+        title: 'Other',
+        rows: [
+          {
+            fields: [
+              {
+                title: 'Description',
+                entityPropertyName: 'description',
+                type: FieldType.Textarea,
+              },
+            ],
+          },
+        ],
+        isOpen: false,
+      },
+    ],
+  };
 
-const App = () => {
+  const values: Person = {};
+
   return <Form schema={formSchema} values={values} />;
 };
 
-export default App;
+export default withTranslation()(App);
