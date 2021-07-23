e78c9914bcc7235d5bf8666e30d4ec3ad940519a
Chao Shi <cshi@bgcengineering.ca>
2021-07-22 21:11:27 -0700

    --

--- src/container/Greeting.tsx
+++ src/container/Greeting.tsx
@@ -1,6 +1,5 @@
 import { whoAmI } from '@src/services';
 import * as React from 'react';
-import { OrderItem } from './OrderItem';
 
 const Greeting = () => {
   const [userName, setUserName] = React.useState<string>();
@@ -26,12 +25,8 @@ const Greeting = () => {
 
   return (
     <>
-      <div className="orderBoard">
-        <OrderItem order={{ price: 11.0, size: 1000 }} />
-        <OrderItem order={{ price: 12.0, size: 1000 }} />
-      </div>
-      {/* {showGreeting() && <h1 data-testid="greeting">Greeting, {userName}</h1>}
-      {!!err && <h1 data-testid="error">{err}</h1>} */}
+      {showGreeting() && <h1 data-testid="greeting">Greeting, {userName}</h1>}
+      {!!err && <h1 data-testid="error">{err}</h1>}
     </>
   );
 };
